0dfa1906b87621d2410af3e9a8e4d59e11fe8f50
kemsakurai <sakurai.kem@mail.com>
2023-01-31 20:58:51 +0900

    Add daisyu.cds

--- packages/client/src/App.vue
+++ packages/client/src/App.vue
@@ -1,11 +1,32 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+<div class="navbar bg-base-100">
+  <div class="navbar-start">
+    <div class="dropdown">
+      <div tabindex="0" class="btn btn-ghost btn-circle" for="menuIcons">
+        <svg xmlns="http://www.w3.org/2000/svg" id="menuIcon" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path
+            stroke-linecap="round" stroke-linejoin="round"
+            stroke-width="2" d="M4 6h16M4 12h16M4 18h7" />
+        </svg>
+      </div>
+      <ul tabindex="0"
+        class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+        <li><router-link to="/">Home</router-link></li>
+        <li><router-link to="/about">About</router-link></li>
+      </ul>
+    </div>
+  </div>
+  <div class="navbar-center">
+    <a class="btn btn-ghost normal-case text-xl">gas-GmailAutoPurge</a>
+  </div>
+  <div class="navbar-end">
+    <button class="btn btn-ghost btn-circle">
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+    </button>
+  </div>
+</div>
+<router-view/>
 </template>
-
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
--- packages/client/src/views/HomeView.vue
+++ packages/client/src/views/HomeView.vue
@@ -1,18 +1,39 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+<div class="overflow-x-auto">
+  <table class="table table-compact w-full">
+      <thead>
+        <th>Leave important email</th>
+        <th>Leave starred email</th>
+        <th>Notes</th>
+        <th>Retention period</th>
+        <th>label</th>
+      </thead>
+      <tbody>
+        <tr v-for='(setting , index) in settings' v-bind:key="index">
+          <td>{{ setting['Leave important email'] }}</td>
+          <td>{{ setting['Leave starred email'] }}</td>
+          <td>{{ setting['Notes'] }}</td>
+          <td>{{ setting['Retention period'] }}</td>
+          <td>{{ setting['label'] }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
-
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { Vue } from 'vue-class-component';
 
-@Options({
-  components: {
-    HelloWorld,
-  },
-})
-export default class HomeView extends Vue {}
+export default class HomeView extends Vue {
+  // Declared as component data
+  settings = [];
+
+  // Declare mounted lifecycle hook
+  mounted() {
+    // eslint-disable-next-line
+    google.script.run.withSuccessHandler((settings) => {
+      this.settings = settings;
+      console.log(this.settings);
+    }).getSettings();
+  }
+}
 </script>
